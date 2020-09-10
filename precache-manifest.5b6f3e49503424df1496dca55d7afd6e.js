d61243e681173faef14c9cc406c4e9e39c8bad10
uschauha <chauhan.s.uday26@gmail.com>
2020-09-10 23:17:38 +0530

    Updates

---
+++ precache-manifest.5b6f3e49503424df1496dca55d7afd6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07867b0e83e78c309e05d005f0b12ff9",
+    "url": "/ucguy4u.github.io/index.html"
+  },
+  {
+    "revision": "1cd099b8b7a6360feb72",
+    "url": "/ucguy4u.github.io/static/css/main.be179ba8.chunk.css"
+  },
+  {
+    "revision": "cf884284eef2072bc16f",
+    "url": "/ucguy4u.github.io/static/js/2.5ea249fd.chunk.js"
+  },
+  {
+    "revision": "7e98744dea5940209892b2e26b1f709f",
+    "url": "/ucguy4u.github.io/static/js/2.5ea249fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd099b8b7a6360feb72",
+    "url": "/ucguy4u.github.io/static/js/main.7f6a19e0.chunk.js"
+  },
+  {
+    "revision": "223d0544cbb8ff618860",
+    "url": "/ucguy4u.github.io/static/js/runtime-main.639be930.js"
+  }
+]);
